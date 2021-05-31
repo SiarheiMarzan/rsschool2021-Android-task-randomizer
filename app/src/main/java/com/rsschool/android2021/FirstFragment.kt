6b17aafc91087574e1789c7c0b3cb4fc6d82f066
Siarhei <siarhei.marzan@gmail.com>
2021-06-01 01:01:37 +0300

    project: add code for start app

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,17 +1,27 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
+    private var listener: ActionGenerate? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as ActionGenerate
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,11 +39,36 @@ class FirstFragment : Fragment() {
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
-        // TODO: val min = ...
-        // TODO: val max = ...
+        val min: EditText = view.findViewById(R.id.min_value)
+        val max: EditText = view.findViewById(R.id.max_value)
+        var minInt = 0
+        var maxInt = 0
+
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
         generateButton?.setOnClickListener {
-            // TODO: send min and max to the SecondFragment
+            if (min.text.toString() != "" && max.text.toString() != "")
+                if (min.text.toString().toLong() <= Int.MAX_VALUE && max.text.toString()
+                        .toLong() <= Int.MAX_VALUE
+                ) {
+                    minInt = min.text.toString().toInt()
+                    maxInt = max.text.toString().toInt()
+                }
+            if (minInt < maxInt) {
+                listener?.onActionGenerate(minInt, maxInt)
+            } else {
+                Toast.makeText(activity, "Входные данные невалидны", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    interface ActionGenerate {
+        fun onActionGenerate(min: Int, max: Int) {
         }
     }
 
--- app/src/main/java/com/rsschool/android2021/SecondFragment.kt
+++ app/src/main/java/com/rsschool/android2021/SecondFragment.kt
@@ -1,17 +1,25 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 
 class SecondFragment : Fragment() {
 
     private var backButton: Button? = null
     private var result: TextView? = null
+    private var listener: ActionBack? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as ActionBack
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,16 +37,28 @@ class SecondFragment : Fragment() {
         val min = arguments?.getInt(MIN_VALUE_KEY) ?: 0
         val max = arguments?.getInt(MAX_VALUE_KEY) ?: 0
 
-        result?.text = generate(min, max).toString()
+        val randomResult = generate(min, max)
+        result?.text = randomResult.toString()
+
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    listener?.backToFirstFragment(randomResult)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
 
         backButton?.setOnClickListener {
-            // TODO: implement back
+            listener?.backToFirstFragment(randomResult)
         }
     }
 
     private fun generate(min: Int, max: Int): Int {
-        // TODO: generate random number
-        return 0
+        return (min..max).random()
+    }
+
+    interface ActionBack {
+        fun backToFirstFragment(randomResult: Int)
     }
 
     companion object {
@@ -47,12 +67,12 @@ class SecondFragment : Fragment() {
         fun newInstance(min: Int, max: Int): SecondFragment {
             val fragment = SecondFragment()
             val args = Bundle()
-
-            // TODO: implement adding arguments
+            args.putInt(MIN_VALUE_KEY, min)
+            args.putInt(MAX_VALUE_KEY, max)
+            fragment.arguments = args
 
             return fragment
         }
-
         private const val MIN_VALUE_KEY = "MIN_VALUE"
         private const val MAX_VALUE_KEY = "MAX_VALUE"
     }
